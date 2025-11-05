e0c3d3e5ff46af8c7e9725149c8fb6cf124e555e
Shuba-Shuba <frankfuffy@gmail.com>
2025-11-04 20:33:33 -0500

    semi-data-driven initial page

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -8,12 +8,21 @@ fetch('pages.json')
       button.id = pages[page].id + "-menubutton";
       button.addEventListener('click', menuButton);
       document.getElementById('menu').appendChild(button);
+      if(pages[page].initial) button.classList.add('selected');
     }
   });
 
 function menuButton() {
   // don't do anything if clicked button's page is already the current page
   if(document.getElementById('current-page-container').children[0].id === this.id) return;
+  
+
+  // unselect old page button
+  document.querySelector('#menu button.selected').classList.remove('selected');
+
+  // select this page button
+  this.classList.add('selected');
+
 
   // move old page from current container to hidden container
   document.getElementById('hidden-page-container').appendChild(document.getElementById('current-page-container').children[0]);
